bb251f9a041d73da080445e4028b8e56c323657b
Jim Sola <james.j.sola@gmail.com>
2021-02-26 18:33:07 -0500

    slight changes to global styles, ensure box-sizing inheritance

--- main/components/Square.js
+++ main/components/Square.js
@@ -1,13 +1,18 @@
 "use strict";
 
+/**
+ * Square object module, inherits Physics module.
+ */
+
 import { Kinetic } from "./Kinetic"
 
 const kin = new Kinetic();
 
-export default function Square(radius){
-    let width = radius;
-    let height = radius;
+export default function Square(size){
+    
+    let com = size/2;
+    
     return(
-        <div style="width:${width};height:${height};border-radius:${radius/2}">testing</div>
+        <div style="width:${size};height:${size};">testing</div>
     )
 }
